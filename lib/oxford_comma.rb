090cb998564f0357da19ab40aa112b4c243772e6
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-18 19:17:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,6 @@ def oxford_comma(array)
   elsif array.length >= 2
     array.join(' and ')
   elsif array.length == 3
-    new_array = array
-    return array.join(' , ')
+    array.join(' , ')
   end
 end
