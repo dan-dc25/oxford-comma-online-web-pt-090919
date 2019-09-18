0b44f550ff4bc26339a7831d7ca311539eb6fa4b
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-18 19:03:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,6 @@ def oxford_comma(array)
     [array].join
   elsif array.length >= 2
     array.join(' and ')
-  elsif array.length == 3
-    array.length(' , ' + ' and ')
   elsif array.length >= 3
  array[-1] = "and #{array[-1]}"
  return array.join(", ")
